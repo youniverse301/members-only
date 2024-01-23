8b19a67457c211cab75ede428f09d92fac05de1d
Bjorn <bjorneng3@gmail.com>
2024-01-23 14:21:31 -0500

    Admins can delete messages

--- express-members-only/routes/index.js
+++ express-members-only/routes/index.js
@@ -5,10 +5,22 @@ var router = express.Router();
 
 router.get('/', asyncHandler(async (req, res, next) => {
   // Fetch all messages from the database
-  const messages = await Message.find().populate('author').exec();
+  const messages = await Message.find().sort({ timestamp: -1 }).populate('author').exec();
 
   // Render the template with the messages
   res.render('index', { user: req.user, title: 'Index', messages });
+}));
+
+// Handle message deletion
+router.post('/delete-message', asyncHandler(async (req, res, next) => {
+  const messageId = req.body.messageId;
+  console.log("test", messageId)
+
+  // Delete the message from the database
+  await Message.findByIdAndDelete(messageId);
+
+  // Redirect back to the index page
+  res.redirect('/');
 }));
 
 module.exports = router;
